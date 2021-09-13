bf09ad9c43e2502026e535c36fb5e8d4926dbd3b
User <User@LAB3-4>
2021-09-13 14:33:03 +0300

    Add project files.

---
+++ GuitarsAndMoreServer/Controllers/GuitarsAndMoreController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GuitarsAndMoreServerBL.Models;
+
+namespace GuitarsAndMoreServer.Controllers
+{
+    [Route("GuitarsAndMoreAPI")]
+    [ApiController]
+    public class GuitarsAndMoreController : ControllerBase
+    {
+        #region Add connection to the db context using dependency injection
+        GuitarsAndMoreDBContext context;
+        public GuitarsAndMoreController(GuitarsAndMoreDBContext context)
+        {
+            this.context = context;
+        }
+        #endregion
+    }
+}
